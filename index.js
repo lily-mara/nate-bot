128f09409289869042edb6f7401591c33b1f0a9c
Nate Mara <natemara@gmail.com>
2016-03-08 19:41:54 -0500

    basic example

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var serverLib = require('./server.js');
 
 var server = serverLib.createServer(8080);
--- server.js
+++ server.js
@@ -1,6 +1,51 @@
 var http = require('http'),
 	winston = require('winston');
 
+const GROUPME_TOKEN = process.env.GROUPME_TOKEN;
+const BOT_ID = process.env.BOT_ID;
+const NATE_ID = process.env.NATE_ID;
+
+function postMessage(botId, text, callback) {
+	var postData = JSON.stringify({
+		bot_id: botId,
+		text: text,
+	});
+
+	var postOptions = {
+		host: 'api.groupme.com',
+		port: '443',
+		path: '/v3/bots/post',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': Buffer.byteLength(postData)
+		}
+	};
+
+	var post_req = http.request(post_options, function(res) {
+		var data = '';
+		res.setEncoding('utf8');
+
+		res.on('data', function (chunk) {
+			data += chunk;
+		});
+
+		res.on('data', function () {
+			winston.info(data);
+			callback(data);
+		});
+	});
+}
+
+function tellNateHeHadAGoodPoint() {
+	postMessage(
+		BOT_ID,
+		"That's a great point Nate!",
+		function (d) {
+			winston.info('Told Nate that he had a great point, got', d);
+		});
+}
+
 /**
  * Creates the server for the pinpoint web service
  * @param {int} port: Port for the server to run on
@@ -16,7 +61,12 @@ exports.createServer = function (port) {
 		});
 
 		request.on('end', function () {
-			winston.info(data);
+			var parsed = JSON.parse(data);
+
+			if (parsed.user_id == NATE_ID) {
+				winston.info('Saw message from Nate, telling him that it was a good point.');
+				tellNateHeHadAGoodPoint();
+			}
 		});
 
 		response.writeHead(501, { 'Content-Type': 'application/json' });
@@ -29,4 +79,5 @@ exports.createServer = function (port) {
 	}
 
 	return server;
+
 };
